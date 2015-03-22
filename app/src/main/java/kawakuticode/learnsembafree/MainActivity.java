72d63543b173df862290ab66a8a28f33f5bd26b3
kawakuticode <kawakuticode@gmail.com>
2015-03-22 16:37:57 +0100

    Version 1.0 Done MotherFucker

--- app/src/main/java/kawakuticode/learnsembafree/MainActivity.java
+++ app/src/main/java/kawakuticode/learnsembafree/MainActivity.java
@@ -1,64 +1,98 @@
 package kawakuticode.learnsembafree;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.view.ViewGroup;
+
+@SuppressLint("NewApi")
+public class MainActivity extends FragmentActivity implements
+        ActionBar.TabListener, View.OnClickListener {
+    private ViewPager viewPager;
+    private TabsPageAdapter mAdapter;
+    //private ActionBar actionBar;
 
 
-public class MainActivity extends ActionBarActivity {
+    // Tab titles
+    private String[] tabs = {"BEGINNER", "INTERMEDIATE", "ADVANCED"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+        //    requestWindowFeature(Window.FEATURE_ACTION_BAR);
 
+        // Initilization
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        //actionBar = getActionBar();
+        mAdapter = new TabsPageAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(mAdapter);
+       /* actionBar.setHomeButtonEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);*/
+
+        // Adding Tabs
+        for (String tab_name : tabs) {
+            actionBar.addTab(actionBar.newTab().setText(tab_name)
+                    .setTabListener(this));
+        }
+
+        /**
+         * on swiping the viewpager make respective tab selected
+         * */
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                // on changing the page
+                // make respected tab selected
+                actionBar.setSelectedNavigationItem(position);
+
+                System.out.println(" ***** My Position **** " + position);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        // on tab selected
+        // show respected fragment view
+        viewPager.setCurrentItem(tab.getPosition());
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+    }
 
-        public PlaceholderFragment() {
-        }
+    @Override
+    public void onClick(View v) {
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
+        if (v.getId() == R.id.footer) {
 
-            return inflater.inflate(R.layout.fragment_main, container, false);
-        }
+            Intent i = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=com.kawakuticode.learnsemba"));
+            startActivity(i);
+
+    }
+
     }
 }
